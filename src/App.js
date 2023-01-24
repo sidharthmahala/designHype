9cd78c1910ec067678e2d89e6c78cecf18c7da9f
Sidharth Mahala <sidharthmahala@gmail.com>
2023-01-24 23:03:07 +0530

    desktop-only

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
- import Hero from './components/Hero';
-// import Services from './components/Services';
-// import StepsMain from './StepsMain';
-// import Ratings from './components/Ratings';
-// import Partners from './components/Partners';
-// import Founder from './components/Founder';
-// import Form from './components/Form';
-// import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import StepsMain from './StepsMain';
+import Ratings from './components/Ratings';
+import Partners from './components/Partners';
+import Founder from './components/Founder';
+import Form from './components/Form';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      
+
       <Topbar />
       <Navbar />
       <Hero />
-      {/*<Ratings />
+      <Ratings />
       <Services />
       <StepsMain />
       <Partners />
       <Founder />
       <Form />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
