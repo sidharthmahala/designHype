bbaaed106bec488ea3d5f16c3404cbba5a7fd360
Sidharth Mahala <sidharthmahala@gmail.com>
2024-05-18 00:41:08 +0530

    design

--- src/Pages/Design.js
+++ src/Pages/Design.js
@@ -7,15 +7,6 @@ import HeroDesign from '../components/DesignSubs/HeroDesign';
 import Navbar from '../components/Home/Navbar'
 import DesignPricing from '../components/DesignSubs/DesignPricing';
 import DesignFSection from '../components/DesignSubs/DesignFSection'
-import HorizontalScrollImages from '../components/DesignSubs/HorizontalScrollImages';
-
-const images = [
-    '../../Assets/Data-report.png',
-    '../../Assets/webdesign.png',
-    '../../Assets/userflow.png',
-    // Add more image URLs as needed
-  ];
-
 
 function Design() {
     return (
@@ -27,8 +18,6 @@ function Design() {
             <DesignFSection />
             <DesignPricing />
             <DesignFSection />
-            <HorizontalScrollImages images={images} scrollSpeed={2} />
-            
         </div>
     )
 }
